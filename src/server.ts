54339e778c43004f37f2ad1d5aa306d8c29a784c
Henrique <Carlos Henrique>
2021-05-17 08:45:41 -0300

    update file index.ts for server.ts

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import { app } from './app';
+
+
+const PORT = process.env.PORT || 3333;
+
+const server = app.listen(PORT, () =>{
+    
+    console.log(`connected server http://localhost:${PORT}`);
+    
+})
+
+/**
+ * Ao finalizar o processo, o server é finalizado também
+ */
+process.on('SIGINT', () => {
+
+    server.close();
+    console.log('server finalizado');
+
+})
