f6824ffab17d748fd7e5d98a8896d6411eabaa4e
Henrique <Carlos Henrique>
2021-05-17 09:56:37 -0300

    Service to create a new user

--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -1,5 +1,19 @@
 import { Router, Request, Response } from 'express';
+import { UsuarioController } from '../controllers/UsuarioController';
+import { Usuario } from '../entity/Usuario';
+
 
 export const routerUsuario = Router();
 
-routerUsuario.get('/', (req: Request, res: Response) => res.send('serviços de usuário'));
+const usuarioCtrl = new UsuarioController();
+
+/**
+ * Serviço para criar um novo usuário
+ */
+routerUsuario.post('/', async (req: Request, res: Response) => {
+    
+    const { nome, email } = req.body;
+    const usuario = new Usuario(nome, email);
+    const usuarioSalvo = await usuarioCtrl.create(usuario);
+    res.json(usuarioSalvo)
+});
