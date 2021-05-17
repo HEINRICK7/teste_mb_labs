c53e4db579a17517de657ba2300dd3e814f53d21
Henrique <Carlos Henrique>
2021-05-17 09:13:09 -0300

    modifield app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import * as cors from 'cors';
 import * as morgan from 'morgan';
 
 import { connectSeverDB } from './config/db';
+import { routerUsuario } from './routes/usuario';
 
 
 export const app = express();
@@ -16,3 +17,6 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 connectSeverDB();
+
+app.use('/usuario', routerUsuario);
+app.use('/', (req: express.Request, res: express.Response) => res.send('API MB_LABS'))
