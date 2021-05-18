57225b0dda176c0276ac1d503397a9dd79ec1baf
Henrique <Carlos Henrique>
2021-05-18 17:18:36 -0300

    authEstabelecimento create

--- src/routes/authEstabelecimento.ts
+++ src/routes/authEstabelecimento.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 import { AuthEstabelecimentoController } from '../controllers/AuthEstabelecimentoController';
-import { routerEstabelecimento } from './estabelecimento';
 
 export const routerAuthEstabelecimento = Router();
 
 const EstabelecimentoCtrl = new AuthEstabelecimentoController();
 
-routerEstabelecimento.post('/', EstabelecimentoCtrl.authenticate);
+routerAuthEstabelecimento.post('/', EstabelecimentoCtrl.authenticate);
